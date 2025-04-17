656a9b019c09b086ed524e10171960c39c09e5ae
Jiten Rai <raijiten96@gmail.com>
2025-04-18 03:31:01 +0545

    minor details

--- padhaisathi/urls.py
+++ padhaisathi/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-import sathi
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('sathi.urls')),
-]
+    path('', include('sathi.urls')),  # Include URLs from your 'sathi' app
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Serve media files during development
